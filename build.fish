24bf8d1908943e68a537c54f3d89e442d7ab45c0
David Stein <davidbstein@gmail.com>
2025-06-25 16:37:08 -0400

    update

--- build.fish
+++ build.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 
-set directory_to_watch _jekyll
+set directory_to_watch "./src"
 
 while true
     inotifywait -r -e modify,create,delete $directory_to_watch
@@ -9,22 +9,5 @@ while true
 
     echo "building site"
 
-    cd _jekyll
-    bundle exec jekyll build --destination ../docs
-    cd ..
-
-
-    echo "setting up 404"
-    echo "---
-permalink: /404.html
----" | cat - docs/404.html > _tmp; mv _tmp docs/404.html
-
-     echo "setting up index"
-     echo "---
-permalink: /index.html
----" | cat - docs/index.html > _tmp; mv _tmp docs/index.html
-
-     echo "setting CNAME"
-     echo "davidbstein.com" > docs/CNAME
-     echo "done"
+    uv run build.py
 end
